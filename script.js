707fa3aa55b71318155c8a157db91caa2e2b44c5
aqsaaqeel <aqsa.aqeel17@gmail.com>
2022-01-10 18:21:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var newQuote = document.querySelector(".new-quote");
+var authorName = document.querySelector(".author-name");
+var quoteButton = document.querySelector(".quote-btn");
+
+function getQuote(){
+    fetch("http://quotes.stormconsultancy.co.uk/random.json")
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+        newQuote.innerText = `" ${data.quote}"`;
+        authorName.innerText = `-${data.author}`;
+
+    });
+}
+
+quoteButton.addEventListener("click", getQuote)
+
